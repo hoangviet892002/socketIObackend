57a8bae3a36b11b3fff81b53cffe56430d54c4b8
viet <viethxse@gmail.com>
2024-06-29 14:24:01 +0700

    update payment and add edit profile

--- routes/auth.js
+++ routes/auth.js
@@ -6,6 +6,7 @@ const {
   logout,
   getProfile,
   updateProfile,
+  changePassword,
 } = require("../controllers/auth.controller");
 const protectRoute = require("../middleware/protectRoute");
 /**
@@ -237,6 +238,65 @@ router.get("/profile", protectRoute, getProfile);
  */
 
 router.post("/update-profile", protectRoute, updateProfile);
+
+/**
+ * @swagger
+ * /auth/change-password:
+ *   post:
+ *     summary: Change password
+ *     tags:
+ *       - Auth
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               oldPassword:
+ *                 type: string
+ *               newPassword:
+ *                 type: string
+ *               confirmPassword:
+ *                 type: string
+ *             required:
+ *               - oldPassword
+ *               - newPassword
+ *               - confirmPassword
+ *             example:
+ *               oldPassword: password123
+ *               newPassword: newpassword123
+ *               confirmPassword: newpassword123
+ *     responses:
+ *       '200':
+ *         description: Change password success
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                 data:
+ *                   type: object
+ *                   properties:
+ *                     _id:
+ *                       type: string
+ *                     fullName:
+ *                       type: string
+ *                     username:
+ *                       type: string
+ *                     profilePic:
+ *                       type: string
+ *                     onSuccess:
+ *                       type: boolean
+ *       '400':
+ *         description: Bad request
+ *       '500':
+ *         description: Internal server error
+ */
+
+router.post("/change-password", protectRoute, changePassword);
 router.post("/logout", logout);
 
 module.exports = router;
