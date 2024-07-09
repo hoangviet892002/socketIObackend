488addb8d730ff176a3d02d42adb0f3217b7e18e
viet <viethxse@gmail.com>
2024-07-09 20:39:59 +0700

    fix get game

--- controllers/game.controller.js
+++ controllers/game.controller.js
@@ -2,32 +2,43 @@ const Game = require("../models/game.model");
 const User = require("../models/user.model");
 
 const getGame = async (req, res) => {
-  const id = req.user._id;
-  const games = await Game.find({
-    $or: [{ loser_id: id }, { winner_id: id }],
-  })
-    .populate({
-      path: "winner_id loser_id",
-      select: "fullName profilePic",
+  try {
+    const id = req.user._id;
+
+    // Fetching games where the user is either the winner or the loser
+    const games = await Game.find({
+      $or: [{ loser_id: id }, { winner_id: id }],
     })
-    .sort({ createdAt: -1 });
+      .populate({
+        path: "winner_id loser_id",
+        select: "fullName profilePic",
+      })
+      .sort({ createdAt: -1 });
 
-  const gameData = games.map((game) => {
-    const competitor =
-      game.winner_id._id === id ? game.loser_id : game.winner_id;
-    const result = game.winner_id._id === id ? "Win" : "Lose";
-    const price = game.gamePrice;
-    const createdAt = game.createdAt;
+    // Formatting the game data
+    const gameData = games.map((game) => {
+      const isWinner = game.winner_id._id.equals(id);
+      const competitor = isWinner ? game.loser_id : game.winner_id;
+      const result = isWinner ? "Win" : "Lose";
 
-    return {
-      competitor,
-      result,
-      price,
-      createdAt,
-    };
-  });
+      return {
+        competitor,
+        result,
+        price: game.gamePrice,
+        createdAt: game.createdAt,
+      };
+    });
 
-  res.status(200).json({ message: "Success", data: gameData, onSuccess: true });
+    // Sending the response
+    res
+      .status(200)
+      .json({ message: "Success", data: gameData, onSuccess: true });
+  } catch (error) {
+    // Handling errors
+    res
+      .status(500)
+      .json({ message: "Internal Server Error", onSuccess: false });
+  }
 };
 
 module.exports = { getGame };
